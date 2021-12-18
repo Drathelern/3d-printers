83461f90bdb7fee7ce8c725956fab16d81e449c0
Edgar Rivas <edgarrivas365@gmail.com>
2021-12-18 16:43:55 -0600

    homming point

--- Ender 3/marlin 2.0.9.2/Marlin/Configuration.h
+++ Ender 3/marlin 2.0.9.2/Marlin/Configuration.h
@@ -1739,8 +1739,8 @@
 #define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
-  #define Z_SAFE_HOMING_X_POINT (((X_CENTER)  / 2) -10)  // X point for Z homing
-  #define Z_SAFE_HOMING_Y_POINT (((Y_CENTER)  / 2) +2)   // Y point for Z homing
+  #define Z_SAFE_HOMING_X_POINT (((X_BED_SIZE) / 2) -10)  // X point for Z homing
+  #define Z_SAFE_HOMING_Y_POINT (((Y_BED_SIZE) / 2) +2 )   // Y point for Z homing
 #endif
 
 // Homing speeds (mm/min)
