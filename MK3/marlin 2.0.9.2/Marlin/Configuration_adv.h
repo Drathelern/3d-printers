5977bb4be7b54d3f92e6158faf300ff61cca52a9
Edgar Rivas <edgarrivas365@gmail.com>
2022-01-09 17:59:07 -0600

    Mk3 extruder: fixed

--- MK3/marlin 2.0.9.2/Marlin/Configuration_adv.h
+++ MK3/marlin 2.0.9.2/Marlin/Configuration_adv.h
@@ -2690,7 +2690,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      560
+    #define E0_CURRENT      900
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
@@ -2837,7 +2837,7 @@
   #define STEALTHCHOP_I
   #define STEALTHCHOP_J
   #define STEALTHCHOP_K
-  #define STEALTHCHOP_E
+  //#define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets
